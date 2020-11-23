e48c1075f10afb538d306272734941958d0ec177
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-11-23 22:00:55 +0200

    Implement compute_contrast().

--- pryzma.py
+++ pryzma.py
@@ -25,3 +25,9 @@ class Color:
             + normalize(self.g) * 0.7152
             + normalize(self.b) * 0.0722
         )
+
+    def compute_contrast(self, color):
+        contrast = (self.relative_luminance + 0.05) / (color.relative_luminance + 0.05)
+        if contrast < 1:
+            return 1 / contrast
+        return contrast
