92eefa97ccfcc6e6cb49923fcf9829c06d3b8863
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-11-23 21:51:14 +0200

    Make use of (cached_)properties.

--- pryzma.py
+++ pryzma.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python
 
-class Color():
-    def __init__(self, r, g, b):
-        self.r, self.g, self.b = r / 255, g / 255, b / 255
+from cached_property import cached_property
 
+
+class Color:
+    def __init__(self, r, g, b):
+        self.__r = r / 255
+        self.__g = g / 255
+        self.__b = b / 255
+
+    r = property(lambda self: self.__r)
+    g = property(lambda self: self.__g)
+    b = property(lambda self: self.__b)
+
+    @cached_property
     def relative_luminance(self):
         def normalize(c):
             if c <= 0.03928:
                 return c / 12.92
-            return ((c + 0.055) / 1.055)**2.4
-        return normalize(self.r) * 0.2126 + \
-               normalize(self.g) * 0.7152 + \
-               normalize(self.b) * 0.0722
+            return ((c + 0.055) / 1.055) ** 2.4
+
+        return (
+            normalize(self.r) * 0.2126
+            + normalize(self.g) * 0.7152
+            + normalize(self.b) * 0.0722
+        )
