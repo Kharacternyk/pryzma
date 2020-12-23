2da407001247ad1790ee6fd4a047b23e478cb90c
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-12-23 16:58:54 +0200

    Add some docstrings.

--- pryzma/color.py
+++ pryzma/color.py
@@ -1,4 +1,5 @@
 def pprint(r, g, b):
+    """Reproduce a color via the terminal escape sequences."""
     switchfg = f"\x1b[38;2;{round(r*255)};{round(g*255)};{round(b*255)}m"
     blocks = "███"
     switchnull = "\x1b[0m"
@@ -6,7 +7,7 @@ def pprint(r, g, b):
 
 
 def hue_normalize(hue):
-    """Normalize an angle to fit within [0; 360)"""
+    """Normalize an angle to fit within [0; 360)."""
     while hue >= 360:
         hue -= 360
     while hue < 0:
@@ -15,6 +16,11 @@ def hue_normalize(hue):
 
 
 def relative_luminance(r, g, b):
+    """Compute the relative luminance of a color.
+
+    The algorithm is described in https://www.w3.org/TR/WCAG20/#relativeluminancedef.
+    """
+
     def normalize(c):
         if c <= 0.03928:
             return c / 12.92
@@ -24,6 +30,10 @@ def relative_luminance(r, g, b):
 
 
 def contrast(rgb1, rgb2):
+    """Compute the contrast ratio of two colors.
+
+    The algorithm is described in https://www.w3.org/TR/WCAG20/#contrast-ratiodef.
+    """
     contrast = (relative_luminance(*rgb1) + 0.05) / (relative_luminance(*rgb2) + 0.05)
     if contrast < 1:
         return 1 / contrast
