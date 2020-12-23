294bd3c3a128a2127798b867c22a74b8365beea7
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-12-23 16:07:29 +0200

    Use a separate file for custom strategies.

---
+++ tests/strategies.py
@@ -0,0 +1,3 @@
+from hypothesis.strategies import floats
+
+channel = floats(min_value=0, max_value=1)
--- tests/test_color.py
+++ tests/test_color.py
@@ -1,8 +1,6 @@
 from hypothesis import given
-from hypothesis.strategies import floats
 from pryzma.color import relative_luminance
-
-channel = floats(min_value=0, max_value=1)
+from strategies import channel
 
 
 @given(channel, channel, channel)
