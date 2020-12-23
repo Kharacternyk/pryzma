3fbb04339721c7e7b12011fbc31fa1b0847f365d
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-12-23 15:23:51 +0200

    Search sampled contrast function for best value.

--- pryzma/palette.py
+++ pryzma/palette.py
@@ -4,20 +4,17 @@ from colorsys import hls_to_rgb
 from pryzma.color import contrast
 
 
-def generate_palette(bg=(1, 1, 1), saturation=1, hue_offset=0, contrast_ratio=5):
+def generate_palette(
+    bg=(1, 1, 1), saturation=1, hue_offset=0, contrast_ratio=5, sample_rate=100
+):
+    yield bg
     hues = ((hue * 60 + hue_offset) / 360 for hue in range(6))
     for hue in hues:
-        print(hue)
-        lightness_upper_bound = 1
-        lightness_lower_bound = 0
-        lightness = (lightness_upper_bound + lightness_lower_bound) / 2
-        while lightness_upper_bound - lightness_lower_bound > 0.01:
-            # Changing bg is unsupported currently
-            if contrast(*hls_to_rgb(hue, lightness, saturation), *bg) >= contrast_ratio:
-                lightness_lower_bound = lightness
-                lightness = (lightness_upper_bound + lightness_lower_bound) / 2
-            else:
-                lightness_upper_bound = lightness
-                lightness = (lightness_upper_bound + lightness_lower_bound) / 2
-            print(lightness_lower_bound, lightness_upper_bound, lightness)
-        yield hls_to_rgb(hue, lightness, saturation)
+        lightness_contrast = (
+            (lightness, contrast(*hls_to_rgb(hue, lightness, saturation), *bg))
+            for lightness in (x / (sample_rate - 1) for x in range(sample_rate))
+        )
+        best_lightness, best_contrast = min(
+            lightness_contrast, key=lambda p: abs(p[1] - contrast_ratio)
+        )
+        yield hls_to_rgb(hue, best_lightness, saturation)
