2da4ff8d3669e372cda4e12fa3bd8efd9007173d
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-12-24 10:59:09 +0200

    Use plural form for strategy names.

--- tests/strategies.py
+++ tests/strategies.py
@@ -1,4 +1,5 @@
-from hypothesis.strategies import floats, tuples
+from hypothesis.strategies import floats
+from hypothesis.strategies import tuples
 
-channel = floats(min_value=0, max_value=1)
-rgb = tuples(channel, channel, channel)
+channels = floats(min_value=0, max_value=1)
+rgb = tuples(channels, channels, channels)
