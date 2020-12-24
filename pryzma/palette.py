79777ce0577a2ef2860c65f79c9ae5361b4f3af1
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-12-24 11:09:20 +0200

    Accept colors in the hex form.

--- pryzma/palette.py
+++ pryzma/palette.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python
-
 from colorsys import hls_to_rgb
 
-from pryzma.color import contrast, hue_normalize, pprint, to_hex
+from pryzma.color import contrast
+from pryzma.color import from_hex
+from pryzma.color import hue_normalize
+from pryzma.color import pprint
+from pryzma.color import to_hex
 
 
 class Palette:
     def __init__(
         self,
-        bg=(0, 0, 0),
-        fg=(0.8, 0.8, 0.8),
+        bg="#EEE8D5",
+        fg="#484848",
         saturation=1,
         hue_offset=0,
         sample_rate=256,
     ):
+        bg = from_hex(bg)
+        fg = from_hex(fg)
         self.__colors = [bg] + [None] * 6 + [fg]
         contrast_ratio = contrast(bg, fg)
         hues = (hue_normalize(hue * 60 + hue_offset) / 360 for hue in range(6))
@@ -27,11 +32,12 @@ class Palette:
                 lightness_contrast, key=lambda p: abs(p[1] / contrast_ratio - 1)
             )
             self.__colors[color] = hls_to_rgb(hue, best_lightness, saturation)
+        self.__contrast_ratio = contrast_ratio
 
     def show(self):
         for c in self.__colors:
             pprint(*c)
-        print()
+        print(f"\nContrast ratio: {self.__contrast_ratio:.1f}")
 
     def print(self):
         for c in self.__colors:
